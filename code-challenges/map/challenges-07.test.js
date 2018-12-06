64c2e60e19ad36fd41cb2037c9164ad4ca3a904d
hsloss <hannahmaguiresloss@gmail.com>
2018-12-06 15:37:49 -0500

    solved problem 2.

--- code-challenges/map/challenges-07.test.js
+++ code-challenges/map/challenges-07.test.js
@@ -26,8 +26,13 @@ Write a function named forEachTwoToThe that produces the same output as your for
 ------------------------------------------------------------------------------------------------ */
 
 const forEachTwoToThe = (arr) => {
-  // Solution code here...
-};
+  let newArr = []
+  let twoPowerOfX = x => Math.pow(2, x);
+  arr.forEach(function(number) {
+    newArr.push(twoPowerOfX(number))
+  })
+  return newArr
+}
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 3
@@ -171,7 +176,7 @@ Run your tests from the console: jest challenges-07.test.js
 
 ------------------------------------------------------------------------------------------------ */
 
-describe.only('Testing challenge 1', () => {
+describe('Testing challenge 1', () => {
   test('It should return two raised to the power of the integer', () => {
     expect(forLoopTwoToThe([0, 4, 5])).toStrictEqual([1, 16, 32]);
     expect(forLoopTwoToThe([0, 4, 5]).length).toStrictEqual(3);
@@ -182,7 +187,7 @@ describe.only('Testing challenge 1', () => {
   });
 });
 
-describe('Testing challenge 2', () => {
+describe.only('Testing challenge 2', () => {
   test('It should return two raised to the power of the integer', () => {
     expect(forEachTwoToThe([0, 4, 5])).toStrictEqual([1, 16, 32]);
     expect(forEachTwoToThe([0, 4, 5]).length).toStrictEqual(3);
